e277965d655ba80151df6861937bde9fdb9bb4b6
Kyenzu <sunadixs99@gmail.com>
2025-05-25 21:17:25 +0800

    Update asd.py

--- asd.py
+++ asd.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import pandas as pd
 from joblib import load
+from category_encoders import BinaryEncoder
+
 
 model = load("decision_tree_model.joblib")
 x = pd.read_csv("dataset.csv")
